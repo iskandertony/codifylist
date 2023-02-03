50249a13a48d109061489896ece6d809e062c1cc
Iska Mabo <maboiskanderr@gmail.com>
2023-02-03 10:52:06 +0600

    final result responsive

--- src/Components/ListFooter/list-footer.jsx
+++ src/Components/ListFooter/list-footer.jsx
@@ -5,25 +5,27 @@ import { ReactComponent as Share } from '../../imgs/share-2.svg'
 import './list-footer.scss'
 const ListFooter = ({ company, created_date, logo, response_count }) => {
     return (
-        <div className="list_container_footer">
-            <div className="list_container_item">
-                <img src={logo} alt="" className="list_container_footer_img" />
-                <div className="list_container_footer_name">{company} </div>
-                <div className="list_container_space">|</div>
-                <div className="list_container_date">
-                    {moment(created_date, 'YYYY-MM-DD').format('DD.MM.YYYY')}
+        <>
+            <div className="list_container_footer">
+                <div className="list_container_item">
+                    <img src={logo} alt="" className="list_container_footer_img" />
+                    <div className="list_container_footer_name">{company} </div>
+                    <div className="list_container_space">|</div>
+                    <div className="list_container_date">
+                        {moment(created_date, 'YYYY-MM-DD').format('DD.MM.YYYY')}
+                    </div>
                 </div>
-            </div>
 
-            <div className="list_container_item">
-                <Icon />
-                <div className="list_container_footer_text">
-                    Откликов:
-                    <span className="list_container_footer_response">{response_count}</span>{' '}
+                <div className="list_container_item">
+                    <Icon />
+                    <div className="list_container_footer_text">
+                        Откликов:
+                        <span className="list_container_footer_response">{response_count}</span>
+                    </div>
+                    <Share />
                 </div>
-                <Share />
             </div>
-        </div>
+        </>
     )
 }
 
